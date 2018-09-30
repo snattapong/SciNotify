560ee909f2bf8d3eefa33150a95a8c8f60de2831
Nattapong Suebsuk <nattapong.s@ubu.ac.th>
2018-09-30 15:56:49 +0700

    Initial commit

---
+++ app/src/main/java/sci/ubu/ac/th/scinotify/Main.java
@@ -0,0 +1,46 @@
+package sci.ubu.ac.th.scinotify;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class Main extends AppCompatActivity {
+
+    TextView mTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //mTextView = findViewById(R.id.msgText);
+    }
+    public void showToken(View view) {
+        // แสดง token มาให้ดูหน่อยเสะ
+        //mTextView.setText(FirebaseInstanceId.getInstance().getToken());
+        Log.i("token", FirebaseInstanceId.getInstance().getToken());
+    }
+    public void subscribeIT(View view) {
+        // สับตะไคร้ it repair
+        FirebaseMessaging.getInstance().subscribeToTopic("repair_it");
+        //mTextView.setText(R.string.it_subscribed);
+    }
+    public void unsubscribeIT(View view) {
+        // ยกเลิกสับตะไคร้ it repair
+        FirebaseMessaging.getInstance().unsubscribeFromTopic("repair_it");
+        //mTextView.setText(R.string.it_un_subscribed);
+    }
+    public void subscribeAsset(View view) {
+        // สับตะไคร้ it repair
+        FirebaseMessaging.getInstance().subscribeToTopic("repair_asset");
+        //mTextView.setText(R.string.asset_subscribed);
+    }
+    public void unsubscribeAsset(View view) {
+        // ยกเลิกสับตะไคร้ it repair
+        FirebaseMessaging.getInstance().unsubscribeFromTopic("repair_asset");
+        //mTextView.setText(R.string.asset_un_subscribed);
+    }
+}
---
+++ app/src/main/java/sci/ubu/ac/th/scinotify/SciFirebaseInstanceID.java
@@ -0,0 +1,22 @@
+package sci.ubu.ac.th.scinotify;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class SciFirebaseInstanceID extends FirebaseInstanceIdService {
+    private static final String LOGTAG = "MyFirebaseID" ;
+
+    @Override
+    public void onTokenRefresh() {
+
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(LOGTAG, "Refreshed token: " + refreshedToken);
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String refreshedToken) {
+        Log.d(LOGTAG,"sendReistationToServer... Leave it here");
+    }
+}
